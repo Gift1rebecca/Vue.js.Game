b6ca040c471f97a9ad8fb1bda9cf903e67cb8ea3
Gift1rebecca <oluwasegungift@gmail.com>
2021-08-16 04:14:19 +0100

    buildup

---
+++ app.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#App',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false
+       },
+    methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth =100;
+        }
+
+         }
+});
